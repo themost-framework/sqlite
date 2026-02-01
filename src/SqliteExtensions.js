72adcb916f23c15bcec6bfc91e3e77705819a22f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2026-02-01 20:52:09 +0200

    resolve __direname when is not available

--- src/SqliteExtensions.js
+++ src/SqliteExtensions.js
@@ -1,9 +1,11 @@
-import path from 'path';
+import {resolve} from 'path';
+// __dirname is not available in ES modules, so we define it here
+const __dirname = __dirname || import.meta.dirname;
 
 const SqliteExtensions = {
-    uuid: path.resolve(__dirname, '../lib/uuid'), // Universally Unique Identifiers
-    crypto: path.resolve(__dirname, '../lib/crypto'), // hashing, encoding and decoding data
-    regexp: path.resolve(__dirname, '../lib/regexp') // regular expressions
+    uuid: resolve(__dirname, '../lib/uuid'), // Universally Unique Identifiers
+    crypto: resolve(__dirname, '../lib/crypto'), // hashing, encoding and decoding data
+    regexp: resolve(__dirname, '../lib/regexp') // regular expressions
 }
 
 export {
